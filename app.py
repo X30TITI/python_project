ad1842a75a6caa13efce74f9bb766d9bdefd227b
boom03 <minhtruong30432003@gmail.com>
2023-12-16 00:56:41 +0700

    push 16-12-2023 12-56

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, flash,render_template, request, redirect, url_for, session, send_from_directory
-from forms import loginForm, AddBook, signUpForm
+from forms import loginForm, AddBook, signUpForm, BorrowBook
 # from flask_uploads import UploadSet, IMAGES, configure_uploads
 import MySQLdb
 from werkzeug.utils import secure_filename
@@ -17,34 +17,15 @@ app.config['MYSQL_DB'] = 'LibraryDB'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
-app.config['UPLOAD_PATH'] = "C:\\Users\\ACER\\Desktop\\BT_Python\\app\\uploads"
-
-
-
-# photos = UploadSet('photos', IMAGES)
-# configure_uploads(app, photos)
-
-
-# @app.route("/add_member")
-# def add_member():
-
-# @app.route("/member")
-# def members():
-
-# @app.route("/delete/<string:id>")
-# def delete_book():
-
+app.config['UPLOAD_PATH'] = "C:\\Users\\ACER\\Desktop\\BT_Python\\app\\static"
 
 @app.route("/add_book", methods=['GET', 'POST'])
 def add_book():
     form = AddBook()
-  
     if request.method == "POST" and form.validate():
         upload_image = form.image.data
-        print(upload_image)
         filenames = secure_filename(upload_image.filename)
         image_path = os.path.join(app.config['UPLOAD_PATH'], filenames)
-       
         upload_image.save(image_path)
         print(upload_image.filename)
         cur = mysql.connection.cursor()
@@ -101,11 +82,15 @@ def viewBook(id):
 @app.route("/edit_book/<string:id>", methods = ["GET", "POST"])
 def edit_book(id):
     form = AddBook()
-   
     cur = mysql.connection.cursor()
     result = cur.execute("SELECT * FROM books WHERE id=%s", [id])
     book = cur.fetchone()
     if request.method == "POST" and form.validate():
+        upload_image = form.image.data
+        filenames = secure_filename(upload_image.filename)
+        image_path = os.path.join(app.config['UPLOAD_PATH'], filenames)
+        upload_image.save(image_path)
+        print(upload_image.filename)
         if(form.id.data != id):
             result = cur.execute(
                 "SELECT id FROM books WHERE id=%s", [form.id.data]
@@ -122,11 +107,10 @@ def edit_book(id):
             form.publication_date.data,
             form.num_pages.data,
             form.available_quantity.data,
-            form.image_path.data,
+            upload_image.filename,
             id])
         mysql.connection.commit()
         cur.close()
-        # flash("Book Updated", "SuccesS")
         return redirect(url_for("books"))
     return render_template("edit_book.html", form=form, book=book)
 
@@ -147,11 +131,96 @@ def delete_book(id):
     flash("Book Deleted", "success")
     return redirect(url_for("books"))
 
+@app.route("/borrow_book/<string:id>",  methods = ["GET", "POST"])
+def borrow_book(id):
+    form = BorrowBook()
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM books WHERE id=%s", [id])
+    book = cur.fetchone()
+    if request.method == "POST" and form.validate():
+      
+        cur.execute("INSERT INTO transactions(user_id, book_id, borrow_date, due_date) VALUES (%s, %s, %s, %s)", [
+                form.user_id.data,
+                form.book_id.data,
+                form.borrow_date.data,
+                form.due_date.data
+        ])
+        mysql.connection.commit()
+        cur.close()
+        flash("Borrow book", "success")
+        return redirect(url_for("user"))
+    return render_template("borrow_book.html", form=form, book=book)
+
+@app.route("/user_info", methods=["GET", "POST"])
+def user_info():
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM account WHERE role = 'user'")
+    users = cur.fetchall()
+    if result > 0:
+        return render_template("user_info.html", users=users)
+    else:
+        msg = 'Khong co user'
+        return render_template("user_info.html", warning = msg)
+    cur.close()
+
+
+@app.route("/add_user", methods=["GET", "POST"])
+def add_user():
+    form = signUpForm(request.form)
+    if request.method == "POST":
+        userName = request.form["userName"]
+        email = request.form["email"]
+        phonenumber = request.form["phonenumber"]
+        password = request.form["password"]
+        role = request.form["role"]
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT username FROM account")
+        users = str(cur.fetchall())
+        cur.execute("SELECT email FROM account")
+        mails = str(cur.fetchall())
+        if not userName in users and not email in mails:
+            if userName.isascii():
+                cur.execute("INSERT INTO account (username,password,email,phonenumber,role) VALUES (%s, %s, %s, %s, %s)", [
+                userName,
+                password,
+                email,
+                phonenumber,
+                role
+            ])
+                mysql.connection.commit()
+        elif userName in users and email in mails:
+            flash("This username and email is unavailable.", "error")
+        elif not userName in users and email in mails:
+            flash("This email is unavailable.", "error")
+        elif userName in users and not email in mails:
+            flash("This username is unavailable.", "error")
+        return redirect(url_for('user_info'))
+    return render_template("add_user.html", form=form)
+
+
+@app.route("/edit_user/<string:id>", methods=["GET", "POST"])
+def edit_user(id):
+    form = signUpForm(request.form)
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM account WHERE id=%s", [id])
+    user = cur.fetchone()
+    if request.method == "POST" and form.validate():
+        cur.execute("UPDATE account SET username=%s, password=%s, email=%s, phonenumber=%s, role=%s WHERE id=%s", [
+            form.userName.data,
+            form.password.data,
+            form.email.data,
+            form.phonenumber.data,
+            form.role.data,
+            id])
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for("user_info"))
+    return render_template("edit_user.html", form=form, user=user)
+
 
 @app.route("/", methods=["GET", "POST"])
 def layout():
     return render_template("layout.html")
-
 
 @app.route("/admin_home")
 def admin_home():
@@ -226,7 +295,7 @@ def login(direct):
                             return redirect(url_for("admin_home"))
                         elif role_user.lower() == "user":
                             flash(f"Welcome user", "success")
-                            return redirect(url_for("user_home"))
+                            return redirect(url_for("user"))
                     else:
                         flash("Sai mat khau", "error")
     return render_template("login.html", form=form, hideLogin=True)
@@ -242,28 +311,6 @@ def logout():
             return redirect("/")
     return redirect(url_for("login", direct='&'))
 
-
-
-@app.route('/uploads/<filename>')
-def get_file(filename):
-    return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
-
-
-@app.route("/add_book", methods=['GET', 'POST'])
-def upload_image():
-    img_url = None
-    form = AddBook()
-    if form.validate_on_submit():
-        photo = form.image.data
-        filename = photos.save(photo)
-        # img_url = photos.url(filename)
-        file_url = url_for('get_file', filename=filename)
-        print(img_url)
-    else: 
-        file_url = None
-    return render_template('addbook.html', form=form, file_url=file_url)
-
-
 @app.route("/index")
 def index():
     return render_template("index.html")
@@ -271,19 +318,17 @@ def index():
 # def navbar():
 #     return render_template("navbar.html")
 
-
-@app.route("/test")
-def test():
+@app.route("/user")
+def user():
     cur = mysql.connection.cursor()
     result = cur.execute("SELECT id, title, author, category, publication_date, num_pages, available_quantity, image_path FROM books")
     books = cur.fetchall()
     if result > 0:
-        return render_template("test.html", books = books)
+        return render_template("user.html", books = books)
     else:
         msg = "Khong co cuon sach nao"
         return render_template("books.html", msg = msg)
     cur.close()
-
 
 
 @app.route('/uploads/<filename>')
@@ -291,6 +336,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 if __name__ == "__main__":
-    
     app.run(debug=True)
     
--- forms.py
+++ forms.py
@@ -5,7 +5,6 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
-
 class loginForm(Form):
     userName = StringField(
         "Username",
@@ -17,7 +16,6 @@ class loginForm(Form):
         [validators.Length(min=8), validators.InputRequired()],
         render_kw={"placeholder": "password"},
     )
-
 
 class AddBook(FlaskForm):
     id = StringField('Book ID', [validators.Length(min=1, max=11)])
@@ -31,6 +29,16 @@ class AddBook(FlaskForm):
         'Total No. of Books', [validators.NumberRange(min=1)])
     image = FileField('Image', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
 
+class BorrowBook(FlaskForm):
+    user_id = StringField('User ID', [validators.Length(min=1, max=11)])
+    book_id = StringField('Book ID', [validators.Length(min=1, max=11)])
+    borrow_date = DateField(
+        'Borrow Date', [validators.InputRequired()])
+    due_date = DateField(
+        'Due Date', [validators.InputRequired()])
+   
+ 
+
 
 class signUpForm(Form):
     userName = StringField(
@@ -43,7 +51,7 @@ class signUpForm(Form):
         [validators.Length(min=6, max=50), validators.InputRequired()],
         render_kw={"placeholder": "email"},
     )
-    password = PasswordField(
+    password = StringField(
         "Password",
         [
             validators.Length(min=8),
@@ -51,14 +59,14 @@ class signUpForm(Form):
         ],
         render_kw={"placeholder": "password"},
     )
-    phonenumber = IntegerField("Phonenumber",[
-            validators.Length(min=10),
+    phonenumber = StringField("Phonenumber",[
+            validators.Length(max=9),
             validators.InputRequired()],
             render_kw={"placeholder": "phonenumber"},
             )
     
     role = StringField(
-        "passwordConfirm",
+        "Role",
         [
             validators.Length(min=4),
             validators.InputRequired(),
